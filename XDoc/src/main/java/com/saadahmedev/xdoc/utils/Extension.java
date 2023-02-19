486cbac265f2ef9bb9b0065a70637fe541837527
saada <saadahmedscse@gmail.com>
2023-02-19 14:13:35 +0600

    Pdf download functionality completed

--- XDoc/src/main/java/com/saadahmedev/xdoc/utils/Extension.java
+++ XDoc/src/main/java/com/saadahmedev/xdoc/utils/Extension.java
@@ -2,5 +2,5 @@ package com.saadahmedev.xdoc.utils;
 
 public class Extension {
     public static final String PDF_EXTENSION = ".pdf";
-    public static final String IMAGE_EXTENSON = ".png";
+    public static final String IMAGE_EXTENSION = ".png";
 }
