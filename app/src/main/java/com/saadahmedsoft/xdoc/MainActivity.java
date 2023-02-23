af25f19b0650b997e951dd33862cc42ad3e5fae4
saada <saadahmedscse@gmail.com>
2023-02-23 11:08:16 +0600

    removed test codes

--- app/src/main/java/com/saadahmedsoft/xdoc/MainActivity.java
+++ app/src/main/java/com/saadahmedsoft/xdoc/MainActivity.java
@@ -1,13 +1,8 @@
 package com.saadahmedsoft.xdoc;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
-import android.widget.Button;
-
-import com.saadahmedev.xdoc.XDoc;
-import com.saadahmedev.xdoc.utils.DocType;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.btn_download);
-        CardView card = findViewById(R.id.layout_card);
-
-        button.setOnClickListener(v ->
-                XDoc.getInstance(this)
-                        .setFolderName("XX")
-                        .setFileName("A file")
-                        .download(card, DocType.PDF_DOCUMENT));
     }
 }
